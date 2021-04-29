ad4a36e85b1afb70a30b10f3a6c4acc616323187
Debora Lopes <deborasantoslop@gmail.com>
2021-04-29 00:16:20 -0300

    Configuração de variavel de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 api.interceptors.request.use(async (config) => {
